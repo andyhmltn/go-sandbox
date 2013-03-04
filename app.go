6453b93efe357f851e67a4266709b42a4c9d2bde
Andy Hamilton <andyhmltn@gmail.com>
2013-03-04 09:37:50 +0000

    Changed the 'Hello World' example to one that runs... ON THE INTERNET!

--- app.go
+++ app.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"net/http"
 )
 
+func server_config() {
+	server := "localhost"
+	port   := "9999"
+
+	server_string := server+":"+port
+	return server_string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request Served")
+	fmt.Fprintf(w, "Hello HTTP World!")
+}
+
 func main() {
-	fmt.Printf("Hello World! Pi: %g\n", math.Pi);
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(server_config, nil)
+
+	fmt.Println("Server started on port 9999")
 }
