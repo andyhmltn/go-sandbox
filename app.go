05b54f10c11a07f01ce6177ca66449641d6acdfc
Andy Hamilton <andyhmltn@gmail.com>
2013-03-06 12:49:33 +0000

    Fixed a bug with an experiment I was trying out

--- app.go
+++ app.go
@@ -5,22 +5,14 @@ import (
 	"net/http"
 )
 
-func server_config() {
-	server := "localhost"
-	port   := "9999"
-
-	server_string := server+":"+port
-	return server_string
-}
-
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Request Served")
 	fmt.Fprintf(w, "Hello HTTP World!")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(server_config, nil)
-
 	fmt.Println("Server started on port 9999")
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("localhost:9999", nil)	
 }
